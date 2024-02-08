33ccf136f003f518b5be64d6dd1797962c078b97
diegorezm <dmourarezende@gmail.com>
2024-02-08 09:46:57 -0300

    day1

---
+++ day1/index.py
@@ -0,0 +1,11 @@
+import random
+
+name = input("Your name:\n")
+pet = input("The name of your pet:\n")
+
+data = [name, pet]
+
+def get_random_index():
+    return random.randint(0,1)
+
+print(f'Your band name can be {data[get_random_index()]} {data[get_random_index()]}')
