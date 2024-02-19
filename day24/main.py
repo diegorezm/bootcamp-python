9f14b6b117929b5260c0639f926afe8b23dc55bd
diegorezm <dmourarezende@gmail.com>
2024-02-19 12:06:30 -0300

    comment

--- day24/main.py
+++ day24/main.py
@@ -14,6 +14,8 @@ with open("./Input/Letters/starting_letter.txt") as file:
 
 for name in names:
     new_letter = template
+    # for some reason the replace method is not working here, 
+    # so im just replacing the entire thing for now
     new_letter[0] = f"Dear {name}\n"
     with open(f"./Output/ReadyToSend/letter_{name}.txt", "w") as file:
         for line in new_letter:
