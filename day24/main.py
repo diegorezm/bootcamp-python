ae99c63ebfb0e594fbea34e890e47a90bc5e0518
diegorezm <dmourarezende@gmail.com>
2024-02-19 12:00:39 -0300

    day24

---
+++ day24/main.py
@@ -0,0 +1,20 @@
+#TODO: Create a letter using starting_letter.txt 
+#for each name in invited_names.txt
+#Replace the [name] placeholder with the actual name.
+#Save the letters in the folder "ReadyToSend".
+
+with open("./Input/Names/invited_names.txt") as file:
+    names = [line.rstrip() for line in file]
+with open("./Input/Letters/starting_letter.txt") as file:
+    template: list[str] = []
+    for line in file:
+        if line == '':
+            pass
+        template.append(line)
+
+for name in names:
+    new_letter = template
+    new_letter[0] = f"Dear {name}\n"
+    with open(f"./Output/ReadyToSend/letter_{name}.txt", "w") as file:
+        for line in new_letter:
+            file.write(line)
