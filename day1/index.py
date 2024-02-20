5acd23fc34059191bb7cfeb391f0a66ccd07cad4
diegorezm <dmourarezende@gmail.com>
2024-02-20 09:52:57 -0300

    formating

--- day1/index.py
+++ day1/index.py
@@ -5,7 +5,9 @@ pet = input("The name of your pet:\n")
 
 data = [name, pet]
 
-def get_random_index():
-    return random.randint(0,1)
 
-print(f'Your band name can be {data[get_random_index()]} {data[get_random_index()]}')
+def get_random_index():
+    return random.randint(0, 1)
+
+
+print(f"Your band name can be {data[get_random_index()]} {data[get_random_index()]}")
--- day10/index.py
+++ day10/index.py
@@ -1,21 +1,27 @@
 from art import logo
+
 print(logo)
 
-def plus(n1:  int, n2: int):
+
+def plus(n1: int, n2: int):
     """THIS FUNCTION ADDS TWO NUMBERS"""
     return n1 + n2
 
-def minus(n1:  int , n2: int):
+
+def minus(n1: int, n2: int):
     """THIS FUNCTION SUBTRACTS TWO NUMBERS"""
     return n1 - n2
 
-def  times(n1: int, n2: int):
+
+def times(n1: int, n2: int):
     """THIS FUNCTION MULTIPLY TWO NUMBERS"""
     return n1 * n2
 
-def devide(n1: int , n2: int):
+
+def devide(n1: int, n2: int):
     """THIS FUNCTION DIVIDES TWO NUMBERS"""
-    return n1/n2
+    return n1 / n2
+
 
 operations = ["+", "-", "*", "/"]
 
@@ -27,7 +33,7 @@ endgame = False
 while not endgame:
     print("\n".join(operations))
     op = input("Pick an operation: ")
-    while op not in operations: 
+    while op not in operations:
         op = input("Pick a valid operation: ")
     match op:
         case "+":
@@ -44,7 +50,9 @@ while not endgame:
             print(f"{n1} * {n2} = {results}")
         case _:
             print("NOT A VALID OPERATION!")
-    user_continue = input(f"Type 'y' to keep using {n1} and {n2}, type 'n' to start a new calculation. (type exit to get out): ").lower()
+    user_continue = input(
+        f"Type 'y' to keep using {n1} and {n2}, type 'n' to start a new calculation. (type exit to get out): "
+    ).lower()
     while user_continue not in ["y", "n", "exit"]:
         user_continue = input("Not a valid input, please provide 'y' or 'n'.")
     if user_continue == "n":
@@ -52,4 +60,3 @@ while not endgame:
         n2 = int(input("What is the second number? "))
     elif user_continue == "exit":
         endgame = True
-
